0101a2e117234d8bd4ab9f02e4d80cd1aeadfdd1
Mahmoud Azab <mahmoudazaab034@gmail.com>
2026-06-21 17:50:57 +0300

    feat: add product router but not complete the db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,26 @@
 import express, {Request, Response} from 'express';
 import {sampleProducts} from './data';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { productRouter } from './router/productRouter';
+
+dotenv.config();
+
+
+const MONGO_URL = 
+process.env.MONGO_URL || 'mongodb://localhost/e-commerce';
+mongoose.set('strictQuery', true);
+console.log(process.env.MONGODB_URI);
+mongoose
+.connect(MONGO_URL)
+.then(() => {
+    console.log('Database connection successful');
+})
+.catch((error) => {
+    console.log('Database connection failed', error);
+});
+
 
 const app = express();
 
@@ -11,13 +31,10 @@ app.use(
     })
 )
 
-app.get('/api/products', (req: Request, res: Response) => {
-    res.json(sampleProducts);
-}); 
 
-app.get('/api/products/:slug', (req: Request, res: Response) => {
-    res.json(sampleProducts.find((p) => p.slug === req.params.slug));
-})
+
+app.use("/api/products", productRouter);
+
 
 
 const PORT = 4000;
